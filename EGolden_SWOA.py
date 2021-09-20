c7c68d73f9d81efcec2d95960135a9ee61b5c68f
ZongSingHuang <a26833765@gmail.com>
2021-09-21 00:43:02 +0800

    1

--- EGolden_SWOA.py
+++ EGolden_SWOA.py
@@ -12,14 +12,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class EGolden_SWOA():
-    def __init__(self, fit_func, num_dim=30, num_particle=20, max_iter=500, 
-                 b=1, x_max=1, x_min=0, a_max=2, a_min=0, l_max=1, l_min=-1, a2_max=-1, a2_min=-2):
-        self.fit_func = fit_func
-        self.num_dim = num_dim
-        self.num_particle = num_particle
-        self.max_iter = max_iter
-        self.x_max = x_max
-        self.x_min = x_min
+    def __init__(self, fitness, D=30, P=20, G=500, ub=1, lb=0,
+                 b=1, a_max=2, a_min=0, a2_max=-1, a2_min=-2, l_max=1, l_min=-1):
+        self.fitness = fitness
+        self.D = D
+        self.P = P
+        self.G = G
+        self.ub = ub
+        self.lb = lb
         self.a_max = a_max
         self.a_min = a_min
         self.a2_max = a2_max
@@ -28,27 +28,35 @@ class EGolden_SWOA():
         self.l_min = l_min
         self.b = b
         
-        self._iter = 1
-        self.gBest_X = None
-        self.gBest_score = np.inf
-        self.gBest_curve = np.zeros(self.max_iter)
-        self.X = np.random.uniform(low=self.x_min, high=self.x_max, size=[self.num_particle, self.num_dim])
-     
-        score = self.fit_func(self.X)
-        self.gBest_score = score.min().copy()
-        self.gBest_X = self.X[score.argmin()].copy()
-        self.gBest_curve[0] = self.gBest_score.copy()
+        self.gbest_X = np.zeros([self.D])
+        self.gbest_F = np.inf
+        self.loss_curve = np.zeros(self.G)
         
     def opt(self):
+        # 初始化
+        self.X = np.random.uniform(low=self.lb, high=self.ub, size=[self.P, self.D])
         tao = (np.sqrt(5)-1)/2
         x1 = -np.pi+(1-tao)
         x2 = -np.pi+tao*2*np.pi
         
-        while(self._iter<self.max_iter):
-            a = self.a_max - (self.a_max-self.a_min)*(self._iter/self.max_iter)
-            self.obl()
+        # 迭代
+        for g in range(self.G):
+            # OBL
+            self.X, F = self.OBL()
             
-            for i in range(self.num_particle):
+            # 更新最佳解
+            if np.min(F) < self.gbest_F:
+                idx = F.argmin()
+                self.gbest_X = self.X[idx].copy()
+                self.gbest_F = F.min()
+            
+            # 收斂曲線
+            self.loss_curve[g] = self.gbest_F
+            
+            # 更新
+            a = self.a_max - (self.a_max-self.a_min)*(g/self.G)
+            
+            for i in range(self.P):
                 p = np.random.uniform()
                 r1 = np.random.uniform()
                 r2 = np.random.uniform()
@@ -56,54 +64,50 @@ class EGolden_SWOA():
                 C = 2*r2
                 
                 if np.abs(A)>=1:
-                    X_rand = self.X[np.random.randint(low=0, high=self.num_particle, size=self.num_dim), :]
+                    X_rand = self.X[np.random.randint(low=0, high=self.P, size=self.D), :]
                     X_rand = np.diag(X_rand).copy()
                     D = np.abs(C*X_rand - self.X[i, :])
                     self.X[i, :] = X_rand - A*D # (4)
                 else:
                     if p<0.5:
-                        D = np.abs(C*self.gBest_X - self.X[i, :])
-                        self.X[i, :] = self.gBest_X - A*D # (1)
+                        D = np.abs(C*self.gbest_X - self.X[i, :])
+                        self.X[i, :] = self.gbest_X - A*D # (1)
                     else:
-                        R1 = 2*np.pi*np.random.uniform()
-                        R2 = np.pi*np.random.uniform()
-                        self.X[i, :] = self.X[i, :]*np.abs(np.sin(R1)) + \
-                                       R2*np.sin(R1)*np.abs(x1*self.gBest_X-x2*self.X[i, :]) # (9)
+                        r3 = 2*np.pi*np.random.uniform()
+                        r4 = np.pi*np.random.uniform()
+                        self.X[i, :] = self.X[i, :]*np.abs(np.sin(r3)) + \
+                                       r4*np.sin(r3)*np.abs(x1*self.gbest_X-x2*self.X[i, :]) # (9)
             
-            self.X = np.clip(self.X, self.x_min, self.x_max)
+            # 邊界處理
+            self.X = np.clip(self.X, self.lb, self.ub)
                    
-            score = self.fit_func(self.X)
-            if np.min(score) < self.gBest_score:
-                self.gBest_X = self.X[score.argmin()].copy()
-                self.gBest_score = score.min().copy()
-                
-            self.gBest_curve[self._iter] = self.gBest_score.copy()
-            self._iter = self._iter + 1
         
     def plot_curve(self):
         plt.figure()
-        plt.title('loss curve ['+str(round(self.gBest_curve[-1], 3))+']')
-        plt.plot(self.gBest_curve, label='loss')
+        plt.title('loss curve ['+str(round(self.loss_curve[-1], 3))+']')
+        plt.plot(self.loss_curve, label='loss')
         plt.grid()
         plt.legend()
         plt.show()
 
-    def obl(self):
+    def OBL(self):
+        # 產生反向解
         k = np.random.uniform()
         alpha = self.X.min(axis=0)
         beta = self.X.max(axis=0)
-        new_X = k*(alpha+beta)-self.X
-
-        idx_too_low = new_X < self.x_min
-        idx_too_high = new_X > self.x_max
+        obl_X = k*(alpha+beta) - self.X # (5)
         
-        rand_X = np.random.uniform(low=alpha, high=beta, size=[self.num_particle, self.num_dim])
-        new_X[idx_too_high] = rand_X[idx_too_high].copy()
-        new_X[idx_too_low] = rand_X[idx_too_low].copy()
+        # 對反向解進行邊界處理
+        rand_X = np.random.uniform(low=alpha, high=beta, size=[self.P, self.D]) # (6)
+        mask = np.logical_or(obl_X>self.ub, obl_X<self.lb)
+        obl_X[mask] = rand_X[mask].copy()
         
-        self.X = np.concatenate((new_X, self.X), axis=0)
-        score = self.fit_func(self.X)
-        top_k = score.argsort()[:self.num_particle]
-        score = score[top_k].copy()
-        self.X = self.X[top_k].copy()
+        # 取得新解
+        concat_X = np.vstack([obl_X, self.X])
+        F = self.fitness(concat_X)
+        top_idx = F.argsort()[:self.P]
+        top_F = F[top_idx].copy()
+        top_X = concat_X[top_idx].copy()
+        
+        return top_X, top_F
 
